916ed37e4b66d26ade6aed67882581bf698c2b6c
Noah Quinones <noahq07@gmail.com>
2021-09-08 18:54:48 +0000

    Configuring clock

--- ATmega168_prog.c
+++ ATmega168_prog.c
@@ -15,8 +15,6 @@ typedef enum FLASH
     ON,
     OFF
 } FLASH;
-// Currently selected pin
-uint8_t pin_b = 5;
 
 // Simply flash an LED given by the pin
 void
@@ -45,6 +43,7 @@ PCINT2_vect(void)
     // TODO: PCINT2 (Pin Change Interrupt 2) is the ISR controlling interrupt handling
     //       for pin changes PCINT23..16
 }
+
 // Pretty flashing lights!!
 void 
 mustang_lights()
@@ -65,14 +64,36 @@ mustang_lights()
 int 
 main(void)
 {
+    /* Pin Configuration */
     // PORTD Pins 6/7 are configured as input pins (set 0 in data direction register)
     DDRD &= ~_BV(PORTD7) & ~_BV(PORTD6);
+    noop; // Probably not necessary since we are not writing to PORTD pins, but nontheless noop for sync
+    
     // Global Interrupt Enable
     SREG |= _BV(7); // setting I-Bit
+    
+    /* Pin Change Interrupt Enabling */
     // Unmask the Pin Change Interrupt for PCINT23/22
     PCMSK2 |= _BV(DDD7) | _BV(DDD6);
     // Enable Pin Change Interrupts for PCINT23..16 (these interrupts map to PORT D)
     PCICR |= _BV(PCIE2);
+
+
+    // Set timer control register -- setting clock source to clkio / 1024 (from prescaler)
+    TCCR0A = _BV(WGM00) | _BV(WGM01) | 
+    TCCR0B = _BV(CS02) | _BV(CS00) | _BV(WGM02);
+    // Unmask interrupts for TOV0 (timer 0 overflow)
+    TOIE0 = _BV(TOIE0);
+    // Atomic write to 0CR0A register, setting the TOP value for TCNT0
+    // Disable the Global interrupt flag in SREG (This is the reverse process shown above e.g. SREG &= ~_BV(7);
+    // Here we choose to use avr instruction cli (clear global interrupt flag)
+    uint8_t saved_sreg = SREG;
+    // On critical section enter
+    __asm__ __volatile__ ("cli");
+    OCR0A = 0xBB; // arbitrary
+    // On critical section exit    
+    SREG = saved_sreg;
+    
     // Call into flashing lights
     mustang_lights();
     return 0;
